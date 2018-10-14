c75ee13e9622dc4fcce2baf24e2d81ba573d8d80
jpsberlin <jpsberlin@hotmail.com>
2018-10-14 21:03:54 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,79 @@
+
+// new class is YogaSession, with name, time & beginner
+
+YogaSession = class {
+
+	constructor(name, time, beginner) {
+		this.name = (name);
+		this.time = (time);
+		this.beginner = (beginner);
+      
+	}
+
+
+}
+
+// new class is Asana, with name, time & beginner
+
+Asana = class {
+	constructor(name, time, beginner) {
+		this.name = (name);
+		this.time = (time);
+		this.beginner = (beginner);
+      
+	}
+}
+
+
+// new class is Pranayama, with name, time & beginner
+
+Pranayama = class {
+	constructor(name, time, beginner) {
+		this.name = (name);
+		this.time = (time);
+		this.beginner = (beginner);
+      
+	}
+}
+
+
+// new class is Meditation, with name, time & beginner
+
+
+Meditation = class {
+	constructor(name, time, beginner) {
+		this.name = (name);
+		this.time = (time);
+		this.beginner = (beginner);
+      
+	}
+}
+
+
+var YogaSession1 = new YogaSession( ’Session1’, ’60 minute’, ’yes’)
+
+var YogaSession2 = new YogaSession( ’Session2’, ’75 minute’, ’yes’)
+
+var YogaSession3 = new YogaSession( ’Session3’, ’90 minute’, ’no’)
+
+
+ 
+var Asana1 = new Asana ('Surya Namaskar', '3 minute', 'yes')
+
+var Asana2 = new Asana ('Bow Pose', '1 minute', 'yes')
+
+var Asana3 = new Asana ('Boat', '1 minute', 'yes')
+
+var Pranayama1 = new Pranayama ('SaTaNaMa', '1 minute', 'yes')
+
+var Pranayama2 = new Pranayama ('Chandra Pran', '5 minute', 'yes')
+
+var Meditation1 = new Meditation ('SaTaNaMa', '7 minute', 'yes')
+
+var Meditation2 = new Meditation ('Gong Meditation', '11 minute', 'yes')
+
+var Meditation3 = new Meditation ('Heart Meditation', '5 minute', 'yes')
+
+
+
+
