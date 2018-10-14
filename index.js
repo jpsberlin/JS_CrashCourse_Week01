33f006ad8868acca1525ff497f837ad2453e702c
jpsberlin <jpsberlin@hotmail.com>
2018-10-14 21:22:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -50,29 +50,29 @@ Meditation = class {
 }
 
 
-var YogaSession1 = new YogaSession( ’Session1’, ’60 minute’, ’yes’)
+var YogaSession1 = new YogaSession( ’Session1’, ’60’, ’true’)
 
-var YogaSession2 = new YogaSession( ’Session2’, ’75 minute’, ’yes’)
+var YogaSession2 = new YogaSession( ’Session2’, ’75’, ’true’)
 
-var YogaSession3 = new YogaSession( ’Session3’, ’90 minute’, ’no’)
+var YogaSession3 = new YogaSession( ’Session3’, ’90’, ’false’)
 
 
  
-var Asana1 = new Asana ('Surya Namaskar', '3 minute', 'yes')
+var Asana1 = new Asana ('Surya Namaskar', '3', 'true')
 
-var Asana2 = new Asana ('Bow Pose', '1 minute', 'yes')
+var Asana2 = new Asana ('Bow Pose', '1', 'true')
 
-var Asana3 = new Asana ('Boat', '1 minute', 'yes')
+var Asana3 = new Asana ('Boat', '1', 'true')
 
-var Pranayama1 = new Pranayama ('SaTaNaMa', '1 minute', 'yes')
+var Pranayama1 = new Pranayama ('SaTaNaMa', '1', 'true')
 
-var Pranayama2 = new Pranayama ('Chandra Pran', '5 minute', 'yes')
+var Pranayama2 = new Pranayama ('Chandra Pran', '5', 'true')
 
-var Meditation1 = new Meditation ('SaTaNaMa', '7 minute', 'yes')
+var Meditation1 = new Meditation ('SaTaNaMa', '7', 'true')
 
-var Meditation2 = new Meditation ('Gong Meditation', '11 minute', 'yes')
+var Meditation2 = new Meditation ('Gong Meditation', '11', 'flase')
 
-var Meditation3 = new Meditation ('Heart Meditation', '5 minute', 'yes')
+var Meditation3 = new Meditation ('Heart Meditation', '5', 'true')
 
 
 
