4ed2b4af8e1238848912733574bfe65f3b30a72e
jpsberlin <jpsberlin@hotmail.com>
2018-10-14 22:00:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -68,40 +68,40 @@ TechYogi = class {
 
 
 
-var YogaSession1 = new YogaSession( ’Session1’, ’60’, ’true’)
+var YogaSession1 = new YogaSession( ’Session1’, ’60’, ’true’);
 
-var YogaSession2 = new YogaSession( ’Session2’, ’75’, ’true’)
+var YogaSession2 = new YogaSession( ’Session2’, ’75’, ’true’);
 
-var YogaSession3 = new YogaSession( ’Session3’, ’90’, ’false’)
+var YogaSession3 = new YogaSession( ’Session3’, ’90’, ’false’);
 
 
  
-var Asana1 = new Asana ('Surya Namaskar', '3', 'true')
+var Asana1 = new Asana ('Surya Namaskar', '3', 'true');
 
-var Asana2 = new Asana ('Bow Pose', '1', 'true')
+var Asana2 = new Asana ('Bow Pose', '1', 'true');
 
-var Asana3 = new Asana ('Boat', '1', 'true')
-
-
-var Pranayama1 = new Pranayama ('SaTaNaMa', '1', 'true')
-
-var Pranayama2 = new Pranayama ('Chandra Pran', '5', 'true')
+var Asana3 = new Asana ('Boat', '1', 'true');
 
 
-var Meditation1 = new Meditation ('SaTaNaMa', '7', 'true')
+var Pranayama1 = new Pranayama ('SaTaNaMa', '1', 'true');
 
-var Meditation2 = new Meditation ('Gong Meditation', '11', 'flase')
-
-var Meditation3 = new Meditation ('Heart Meditation', '5', 'true')
-
-var armagan = new TechYogis('Armagan', 34)
-var omar = new TechYogis('Omar', 30)
-var armagan = new Person('Anna', 27)
-var maria = new Person('Maria', 26)
+var Pranayama2 = new Pranayama ('Chandra Pran', '5', 'true');
 
 
+var Meditation1 = new Meditation ('SaTaNaMa', '7', 'true');
 
-armagan.attend(YogaSession1)
-omar.attend(YogaSession2)
-anna.attend(YogaSession3)
-maria.attend(YogaSession1)
+var Meditation2 = new Meditation ('Gong Meditation', '11', 'flase');
+
+var Meditation3 = new Meditation ('Heart Meditation', '5', 'true');
+
+var armagan = new TechYogis('Armagan', 34);
+var omar = new TechYogis('Omar', 30);
+var armagan = new Person('Anna', 27);
+var maria = new Person('Maria', 26);
+
+
+
+armagan.attend(YogaSession1);
+omar.attend(YogaSession2);
+anna.attend(YogaSession3);
+maria.attend(YogaSession1);
