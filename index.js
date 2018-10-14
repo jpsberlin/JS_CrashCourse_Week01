cba073f547e9b34871d4d961f826b7b119968fa9
jpsberlin <jpsberlin@hotmail.com>
2018-10-14 22:36:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -67,36 +67,36 @@ TechYogi = class {
 
 
 
-var yogaSession1 = new YogaSession( ’YogaSession1’, 60, true);
+var yogaSession1 = new YogaSession( "YogaSession1", 60, true);
 
-var yogaSession2 = new YogaSession( ’YogaSession2’, 7, true);
+var yogaSession2 = new YogaSession( "YogaSession2", 7, true);
 
-var yogaSession3 = new YogaSession( ’YogaSession3’, 90, false);
+var yogaSession3 = new YogaSession( "YogaSession3", 90, false);
 
 
  
-var Asana1 = new Asana ('SuryaNamaskar', 3, true);
+var Asana1 = new Asana ("SuryaNamaskar", 3, true);
 
-var Asana2 = new Asana ('BowPose', 1, true);
+var Asana2 = new Asana ("BowPose", 1, true);
 
-var Asana3 = new Asana ('Boat', 1, true);
-
-
-var Pranayama1 = new Pranayama ('SatnamaPran', 1, true);
-
-var Pranayama2 = new Pranayama ('ChandraPran', 5, true);
+var Asana3 = new Asana ("Boat", 1, true);
 
 
-var Meditation1 = new Meditation ('SaTaNaMa', 7, true);
+var Pranayama1 = new Pranayama ("SatnamaPran", 1, true);
 
-var Meditation2 = new Meditation ('GongMeditation', 11, false);
+var Pranayama2 = new Pranayama ("ChandraPran", 5, true);
 
-var Meditation3 = new Meditation ('Heart Meditation', 5, true);
 
-var armagan = new TechYogis('Armagan', 34);
-var omar = new TechYogis('Omar', 30);
-var armagan = new Person('Anna', 27);
-var maria = new Person('Maria', 26);
+var Meditation1 = new Meditation ("SaTaNaMa", 7, true);
+
+var Meditation2 = new Meditation ("GongMeditation", 11, false);
+
+var Meditation3 = new Meditation ("Heart Meditation", 5, true);
+
+var armagan = new TechYogis("Armagan", 34);
+var omar = new TechYogis("Omar", 30);
+var armagan = new Person("Anna", 27);
+var maria = new Person("Maria", 26);
 
 
 
